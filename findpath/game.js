fd52824e7e8f75bb73f8a30017acb55c0bd3b2f7
QuXinbo <2816095378@qq.com>
2016-03-23 20:29:57 +0800

    作业四

---
+++ findpath/game.js
@@ -0,0 +1,105 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var game;
+(function (game) {
+    var GRID_PIXEL_WIDTH = 50;
+    var GRID_PIXEL_HEIGHT = 50;
+    var NUM_ROWS = 12;
+    var NUM_COLS = 12;
+    var WorldMap = (function (_super) {
+        __extends(WorldMap, _super);
+        function WorldMap() {
+            _super.call(this);
+            var grid = new astar.Grid(NUM_COLS, NUM_ROWS);
+            this.grid = grid;
+            this.grid.setWalkable(5, 0, false);
+            this.grid.setWalkable(5, 1, false);
+            this.grid.setWalkable(5, 2, false);
+            this.grid.setWalkable(5, 3, false);
+            this.grid.setWalkable(5, 4, false);
+            this.grid.setWalkable(5, 5, false);
+        }
+        WorldMap.prototype.render = function (context) {
+            context.strokeStyle = '#FF0000';
+            context.beginPath();
+            for (var i = 0; i < NUM_COLS; i++) {
+                for (var j = 0; j < NUM_ROWS; j++) {
+                    if (this.grid.getNode(i, j).walkable == false) {
+                        context.fillStyle = '#000000';
+                        context.fillRect(i * GRID_PIXEL_WIDTH, j * GRID_PIXEL_HEIGHT, GRID_PIXEL_WIDTH, GRID_PIXEL_HEIGHT);
+                        context.fillRect(i * GRID_PIXEL_WIDTH, (j - 1) * GRID_PIXEL_HEIGHT, GRID_PIXEL_WIDTH, GRID_PIXEL_HEIGHT);
+                        context.beginPath();
+                    }
+                    if (this.grid.getNode(i, j).walkable == true) {
+                        context.fillStyle = '#0000FF';
+                        context.rect(i * GRID_PIXEL_WIDTH, j * GRID_PIXEL_HEIGHT, GRID_PIXEL_WIDTH, GRID_PIXEL_HEIGHT);
+                    }
+                    context.fill();
+                    context.stroke();
+                    context.closePath();
+                }
+            }
+        };
+        return WorldMap;
+    }(DisplayObject));
+    game.WorldMap = WorldMap;
+    var BoyShape = (function (_super) {
+        __extends(BoyShape, _super);
+        function BoyShape() {
+            _super.apply(this, arguments);
+        }
+        BoyShape.prototype.render = function (context) {
+            context.beginPath();
+            context.fillStyle = '#00FFFF';
+            context.arc(GRID_PIXEL_WIDTH / 2, GRID_PIXEL_HEIGHT / 2, Math.min(GRID_PIXEL_WIDTH, GRID_PIXEL_HEIGHT) / 2 - 5, 0, Math.PI * 2);
+            context.fill();
+            context.closePath();
+        };
+        return BoyShape;
+    }(DisplayObject));
+    game.BoyShape = BoyShape;
+    var BoyBody = (function (_super) {
+        __extends(BoyBody, _super);
+        function BoyBody() {
+            _super.apply(this, arguments);
+            this.X = new Array();
+            this.Y = new Array();
+            this.l = 1;
+        }
+        BoyBody.prototype.run = function (grid) {
+            grid.setStartNode(0, 0);
+            grid.setEndNode(10, 8);
+            var findpath = new astar.AStar();
+            findpath.setHeurisitic(findpath.diagonal);
+            var result = findpath.findPath(grid);
+            var path = findpath._path;
+            for (var i = 0; i < path.length; i++) {
+                this.X[i] = path[i].x;
+                this.Y[i] = path[i].y;
+            }
+            console.log(path);
+            console.log(grid.toString());
+        };
+        BoyBody.prototype.onTicker = function (duringTime) {
+            if (this.x < NUM_ROWS * GRID_PIXEL_WIDTH && this.y < NUM_COLS * GRID_PIXEL_HEIGHT) {
+                this.x = this.X[this.l] * GRID_PIXEL_WIDTH;
+                this.y = this.Y[this.l] * GRID_PIXEL_HEIGHT;
+                this.l++;
+                console.log(this.x, this.y);
+            }
+        };
+        return BoyBody;
+    }(Body));
+    game.BoyBody = BoyBody;
+})(game || (game = {}));
+var boyShape = new game.BoyShape();
+var world = new game.WorldMap();
+var body = new game.BoyBody(boyShape);
+body.run(world.grid);
+var renderCore = new RenderCore();
+renderCore.start([world, boyShape]);
+var ticker = new Ticker();
+ticker.start([body]);
